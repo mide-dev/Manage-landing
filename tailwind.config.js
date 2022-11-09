98c2978fc4fd21bb0c4d584322aff1c3a86b1fc6
midedev <ibrahimayomide330@gmail.com>
2022-11-09 09:06:25 +0100

    feat:add custom colors and font to root

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBlue: "hsla(228, 38%, 23%, 1)",
+        customOrange: "hsla(12, 88%, 59%, 1)",
+        customBlack: "hsla(232, 12%, 13%, 1)",
+        customLightPink: "hsla(13, 100%, 96%, 1)",
+        customWhite: "hsla(9, 54%, 97%, 1)",
+        // customOrangeAccent: "#F3603C",
+      },
+      fontFamily: {
+        beVietnam: ["Be Vietnam Pro", "serif"],
+      },
+    },
   },
   plugins: [],
 };
