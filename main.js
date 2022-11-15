c5581a124baf941cab3c03f2733d8ac14be1fc85
midedev <ibrahimayomide330@gmail.com>
2022-11-15 06:48:10 +0100

    styles:add comments

--- main.js
+++ main.js
@@ -4,13 +4,16 @@ const closeIcon = document.querySelector(".icon-close");
 const navWrapper = document.querySelector(".nav-wrapper");
 const mainHeader = document.querySelector(".main-header");
 
+// handle click event for mobile menu toggle
 mobileNavMenu.addEventListener("click", () => {
   hamburgerIcon.classList.toggle("hidden");
   closeIcon.classList.toggle("hidden");
 
+  // toggle navbar on click
   hamburgerIcon.classList.contains("hidden")
     ? navWrapper.classList.remove("hidden")
     : navWrapper.classList.add("hidden");
 
+  // add background overlay while menu is active
   mainHeader.toggleAttribute("data-overlay");
 });
