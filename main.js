3b9ee77db7247ba77890f48f2a4219a6d90601aa
midedev <ibrahimayomide330@gmail.com>
2022-11-15 06:38:49 +0100

    feat:add backgound overlay when mobile menu active

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const mobileNavMenu = document.querySelector(".mobile-nav");
 const hamburgerIcon = document.querySelector(".icon-hamburger");
 const closeIcon = document.querySelector(".icon-close");
 const navWrapper = document.querySelector(".nav-wrapper");
+const mainHeader = document.querySelector(".main-header");
 
 mobileNavMenu.addEventListener("click", () => {
   hamburgerIcon.classList.toggle("hidden");
@@ -10,4 +11,6 @@ mobileNavMenu.addEventListener("click", () => {
   hamburgerIcon.classList.contains("hidden")
     ? navWrapper.classList.remove("hidden")
     : navWrapper.classList.add("hidden");
+
+  mainHeader.toggleAttribute("data-overlay");
 });
